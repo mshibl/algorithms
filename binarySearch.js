153fac2bb0a29f12eb1c6f549acc5f8c202f8202
mshibl <mohamed.shibl@hotmail.com>
2016-01-11 21:46:53 -0800

    adds more clarity to the binarySearch code

--- binarySearch.js
+++ binarySearch.js
@@ -1,24 +1,32 @@
 /* Returns either the index of the location in the array,
   or -1 if the array did not contain the targetValue */
 var doSearch = function(array, targetValue) {
-	var min = 0;
-	var max = array.length - 1;
+    var min = 0;
+    var max = array.length - 1;
     var guess;
+    var guessCount = 0;
     
     while(min<=max){
         guess = Math.floor((max+min)/2);
-        if(array[guess] === targetValue){return guess;} 
+        println(guess);
+        guessCount += 1;
+        if(array[guess] === targetValue){
+            println(guessCount);
+            return guess;
+            
+        } 
         else if(array[guess]<targetValue) {min = guess + 1;}
         else {max = guess - 1;}
     }
-
-	return -1;
+    return -1;
 };
 
 var primes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 
-		41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97];
+        41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97];
 
 var result = doSearch(primes, 73);
 println("Found prime at index " + result);
 
 Program.assertEqual(doSearch(primes, 73), 20);
+Program.assertEqual(doSearch(primes, 53), 15);
+Program.assertEqual(doSearch(primes, 2), 0);
